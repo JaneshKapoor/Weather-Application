586f993ba44707d0a960d76f5a1de3ef923ac661
Janesh Kapoor <kapoorjanesh.2003@gmail.com>
2023-07-18 14:05:27 +0530

    Setup of RapidAPI

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const url = 'https://air-quality-by-api-ninjas.p.rapidapi.com/v1/airquality?city=Seattle';
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'e1f0fc39d5mshe187762bad1b92dp1cc5c7jsncd73594b6603',
-		'X-RapidAPI-Host': 'air-quality-by-api-ninjas.p.rapidapi.com'
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
 
 try {
 	const response = await fetch(url, options);
 	const result = await response.text();
+    
+    cloud_pct = result.cloud_pct
+    temp = result.temp
+    feels_like = result.feels_like
+    humidity = result.humidity
+    min_temp = result.min_temp
+    max_temp = result.max_temp
+    wind_speed09 = result.wind_speed09
+    wind_degrees = result.wind_degrees
+    sunrise = result.sunrise
+    sunset = result.sunset
+    
 	console.log(result);
 } catch (error) {
 	console.error(error);
