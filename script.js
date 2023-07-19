39cb0c1b52df0017550c6cb91d402ec8f30d4bf4
Janesh Kapoor <kapoorjanesh.2003@gmail.com>
2023-07-20 00:12:20 +0530

    Updates pushed

--- script.js
+++ script.js
@@ -6,19 +6,31 @@ const options = {
     }
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi',options)
+const getWeather = (city) => {
+
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city ,options)
     .then(response => response.json())
     .then(response => {console.log(response)
+    cityname.innerHTML = city + " ";
     cloud_pct.innerHTML = response.cloud_pct
     temp.innerHTML = response.temp
     feels_like.innerHTML = response.feels_like
     humidity.innerHTML = response.humidity
     min_temp.innerHTML = response.min_temp
     max_temp.innerHTML = response.max_temp
-    wind_speed09.innerHTML = response.wind_speed09
+    wind_speed09.innerHTML = response.wind_speed
     wind_degrees.innerHTML = response.wind_degrees
     sunrise.innerHTML = response.sunrise
-    sunset.innerHTML = response.sunset   
+    sunset.innerHTML = response.sunset  
     })
     
     .catch(err => console.error(err))
+
+}
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value);
+})
+
+getWeather("Delhi");
