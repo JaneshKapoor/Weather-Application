58241d9bfa5875a9257eec05ff26a00862e3db63
Janesh Kapoor <kapoorjanesh.2003@gmail.com>
2023-07-27 01:44:13 +0530

    Application updated

--- script.js
+++ script.js
@@ -57,17 +57,59 @@ const tableupdate = (city) => {
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city ,options)
     .then(response => response.json())
     .then(response => {console.log(response)
-    // delhidata.innerHTML = city + " ";
-    delhicloud.innerHTML = response.cloud_pct
-    delhidata.innerHTML = response.temp
-    delhifeelslike.innerHTML = response.feels_like
-    delhihumidity.innerHTML = response.humidity
-    delhimintemp.innerHTML = response.min_temp
-    delhimaxtemp.innerHTML = response.max_temp
-    delhiwindspeed.innerHTML = response.wind_speed
-    delhiwinddegrees.innerHTML = response.wind_degrees
-    delhisunrise.innerHTML = response.sunrise
-    delhisunset.innerHTML = response.sunset  
+    
+        if(city == "Delhi"){
+            // delhidata.innerHTML = city + " ";
+        delhicloud.innerHTML = response.cloud_pct
+        delhidata.innerHTML = response.temp
+        delhifeelslike.innerHTML = response.feels_like
+        delhihumidity.innerHTML = response.humidity
+        delhimintemp.innerHTML = response.min_temp
+        delhimaxtemp.innerHTML = response.max_temp
+        delhiwindspeed.innerHTML = response.wind_speed
+        delhiwinddegrees.innerHTML = response.wind_degrees
+        delhisunrise.innerHTML = response.sunrise
+        delhisunset.innerHTML = response.sunset 
+        } 
+        else if(city == "Bangalore"){
+            Bangalorecloud.innerHTML = response.cloud_pct
+        Bangaloredata.innerHTML = response.temp
+        Bangalorefeelslike.innerHTML = response.feels_like
+        Bangalorehumidity.innerHTML = response.humidity
+        Bangaloremintemp.innerHTML = response.min_temp
+        Bangaloremaxtemp.innerHTML = response.max_temp
+        Bangalorewindspeed.innerHTML = response.wind_speed
+        Bangalorewinddegrees.innerHTML = response.wind_degrees
+        Bangaloresunrise.innerHTML = response.sunrise
+        Bangaloresunset.innerHTML = response.sunset
+        }
+
+        else if(city == "Mumbai"){
+            Mumbaicloud.innerHTML = response.cloud_pct
+        Mumbaidata.innerHTML = response.temp
+        Mumbaifeelslike.innerHTML = response.feels_like
+        Mumbaihumidity.innerHTML = response.humidity
+        Mumbaimintemp.innerHTML = response.min_temp
+        Mumbaimaxtemp.innerHTML = response.max_temp
+        Mumbaiwindspeed.innerHTML = response.wind_speed
+        Mumbaiwinddegrees.innerHTML = response.wind_degrees
+        Mumbaisunrise.innerHTML = response.sunrise
+        Mumbaisunset.innerHTML = response.sunset
+        }
+
+        else if(city == "Kolkata"){
+        Kolkatacloud.innerHTML = response.cloud_pct
+        Kolkatadata.innerHTML = response.temp
+        Kolkatafeelslike.innerHTML = response.feels_like
+        Kolkatahumidity.innerHTML = response.humidity
+        Kolkatamintemp.innerHTML = response.min_temp
+        Kolkatamaxtemp.innerHTML = response.max_temp
+        Kolkatawindspeed.innerHTML = response.wind_speed
+        Kolkatawinddegrees.innerHTML = response.wind_degrees
+        Kolkatasunrise.innerHTML = response.sunrise
+        Kolkatasunset.innerHTML = response.sunset
+        }
+
     })
     
     .catch(err => console.error(err)) 
@@ -75,3 +117,5 @@ const tableupdate = (city) => {
 
 tableupdate("Delhi");
 tableupdate("Bangalore");
+tableupdate("Mumbai");
+tableupdate("Kolkata");
