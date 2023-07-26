c729a0dd886fa1d63591489aa8b488284a38a84b
Janesh Kapoor <kapoorjanesh.2003@gmail.com>
2023-07-27 01:01:36 +0530

    Updates commit

--- script.js
+++ script.js
@@ -52,3 +52,26 @@ Mumbai.addEventListener("click",(e) => {
 
 
 getWeather("Delhi");
+
+const tableupdate = (city) => {
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city ,options)
+    .then(response => response.json())
+    .then(response => {console.log(response)
+    // delhidata.innerHTML = city + " ";
+    delhicloud.innerHTML = response.cloud_pct
+    delhidata.innerHTML = response.temp
+    delhifeelslike.innerHTML = response.feels_like
+    delhihumidity.innerHTML = response.humidity
+    delhimintemp.innerHTML = response.min_temp
+    delhimaxtemp.innerHTML = response.max_temp
+    delhiwindspeed.innerHTML = response.wind_speed
+    delhiwinddegrees.innerHTML = response.wind_degrees
+    delhisunrise.innerHTML = response.sunrise
+    delhisunset.innerHTML = response.sunset  
+    })
+    
+    .catch(err => console.error(err)) 
+}
+
+tableupdate("Delhi");
+tableupdate("Bangalore");
